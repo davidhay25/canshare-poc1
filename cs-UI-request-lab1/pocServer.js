21d61306718e6f81e678e5256587abd75e28245e
davidhay25 <david.hay25@gmail.com>
2025-04-01 15:18:06 +1300

    auto select root

--- cs-UI-request-lab1/pocServer.js
+++ cs-UI-request-lab1/pocServer.js
@@ -66,6 +66,8 @@ app.use((req, res, next) => {
     next();
 });
 
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 /*
 //disable any cache - https://stackoverflow.com/questions/22632593/how-to-disable-webpage-caching-in-expressjs-nodejs
